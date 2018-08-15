2480e9fabcf5859811aee08edabfefcaf66e3b9c
Julie Laursen <Julielaursen1@gmail.com>
2018-08-14 21:58:54 -0500

    First unit test

--- src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
+++ src/test/java/com/bharath/product/dao/ProductDAOImplTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.bharath.product.dto.Product;
+
 public class ProductDAOImplTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createShouldCreateAProduct() {
+		ProductDAO dao = new ProductDAOImpl();
+		Product product = new Product();
+		product.setId(1);
+		product.setName("iphone");
+		product.setDescription("it's awesome");
+		product.setPrice(800);
+		dao.create(product);
+		Product result = dao.read(1);
+		
+		assertNotNull(result);
+		assertEquals("iphone", result.getName());
 	}
 
 }
